44a9a995b2355b5be33a371a705d6553e3b83190
kuriringohankamehameha <kuriringohankamehameha@github.com>
2019-09-06 15:28:57 +0530

    Added basic functionality for the Shell

--- Load Balancer/README.md
+++ Load Balancer/README.md
@@ -45,3 +45,7 @@
 ```
 
 * The above would compile and run the program that reads from the file "sample.txt" and the Number of Children (N) = 5.
+
+# Regarding Plagiarism
+
+* I pledge that I have written most of the code by myself, along with a few helpful inputs and snippets from www.stackoverflow.com. For example, I've taken the `sigaction` call from stackoverflow, as I needed a reference. And the code involved in the question https://stackoverflow.com/questions/57620234/how-to-keep-sending-signals-to-children-from-parent-process-repeatedly is from me. I am the one who asked this question, so if the plagiarism checker detected that it matched with this website, I can provide relevant proof regarding the same.
--- MyShell/shell.c
+++ MyShell/shell.c
@@ -1,16 +1,78 @@
 #include "shell.h"
 
+#define MAX_NUM_PATHS 100
+
+char** PATH;
+int PATH_LENGTH;
+
+//Current Child PID
+int CURR_CHILD_PID = -1;
+int PARENT_PID;
+
+//Shell Signal Handler
+static void shell_handler(int);
+
+static void shell_handler(int signo)
+{
+    //Pass the signals to the process running in the shell
+    if (CURR_CHILD_PID > 0)
+    {
+        kill(CURR_CHILD_PID, signo);
+        kill(PARENT_PID, SIGCHLD);
+    }
+    else
+        printf("\n");
+
+}
+
+void initialize_path(const char* filename)
+{
+    //Sets the PATH from filename (the Shell config file)
+    PATH = (char**) malloc (MAX_NUM_PATHS*sizeof(char*));
+    for(int i=0; i<MAX_NUM_PATHS; i++)
+        PATH[i] = (char*) malloc (sizeof(char));
+
+    char buffer[256];
+    int k = 0;
+    int j = 0;
+    FILE* file = fopen(filename, "r");
+    
+    if (!file)
+    {
+        printf("Error opening file '%s'\n", filename);
+        exit(1);
+    }
+
+    while(fgets(buffer, sizeof(buffer), file))
+    {
+        //printf("buffer = %s",buffer);
+        for(int i=0; ; i++)
+        {
+            if(buffer[i] == '\n')
+            {
+                PATH[j][k] = '\0';
+                break;
+            }
+
+            else
+                PATH[j][k++] = buffer[i];
+        }
+
+        j++;
+        k = 0;
+    }
+
+    PATH_LENGTH = j;
+}
+
 int compare_strs(char* s1, char* s2)
 {
     if(strlen(s1) != strlen(s2))
         return 0;
 
     for(int i=0; i<strlen(s1); i++)
-    {
         if(s1[i] != s2[i])
             return 0;
-    }
-
     return 1;
 }
 
@@ -20,12 +82,10 @@ char* str_concat(char* s1, char* s2)
     int l2 = strlen(s2);
     char* op = (char*) malloc ((l1 + l2)*sizeof(char));
     for(int i=0; i<l1 + l2; i++)
-    {
         if (i < l1)
             op[i] = s1[i];
         else
             op[i] = s2[i-l1];
-    }
     return op;
 }
 
@@ -33,26 +93,50 @@ int get_args(char* s)
 {
     int c = 0;
     for(int i=0; s[i]!='\0'; i++)
-    {
         if(s[i] == ' ')
-        {
             c++;
-        }
+    return c;
+}
+
+void clear_terminal()
+{
+    for(int i=0; i<200; i++)
+        printf("\n");
+}
+
+int custom_command(char* command)
+{
+    if(strcmp(command, "clear") == 0)
+    {
+       clear_terminal();    
+       return 1;
     }
 
-    return c;
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
+    //Set the PATH first
+    initialize_path(".myshellrc");
+
     char c;
-    //char* buf = (char*) malloc (sizeof(char));
     char* buf = NULL;
     size_t n = 0;
     char* exit_str = "exit"; 
+    
+    printf("myShell~:$ ");
+
+    struct sigaction sa;
+    sa.sa_handler = shell_handler;
+
+    //sigaction(SIGINT, &sa, NULL);
+
+    int pipefd[2];
+    pipe(pipefd);
+
     while((c = getline(&buf, &n, stdin)))
     {
-        
         for(int i=0; buf[i]!='\0'; i++)
             if(buf[i] == '\n')
                 buf[i] = '\0';
@@ -62,55 +146,79 @@ int main(int argc, char* argv[])
             free(buf);
             exit(0);
         }
-
-        int pid = fork();
-
-        if(pid == 0)
+        
+        if(custom_command(buf) == 1)
         {
-            char* op;
-           
-            int ARG_LENGTH = get_args(buf) + 1;
-            char** arg = (char**) malloc (ARG_LENGTH*sizeof(char*));
-            for(int i=0; i<ARG_LENGTH; i++)
-            {
-                arg[i] = (char*) malloc (70*sizeof(char));
-            }
-
-            int count = 0;
-            int j=0;
-            for(int i=0; buf[i]!='\0'; i++)
-            {
-                if(buf[i] == ' ')
-                {
-                    arg[count][j] = '\0';
-                    j = 0;
-                    count ++;
-                }
-
-                else
-                {
-                    arg[count][j++] = buf[i];
-                }
-            }
-            
-            arg[count][j] = '\0';
-            arg[count+1] = NULL;
-
-            execv(op=str_concat("/bin/",arg[0]), arg);
-            free(op);
-            for(int i=0; i<ARG_LENGTH; i++)
-            {
-                free(arg[i]);
-            }
-            free(arg);
-            perror("Error:");
-            exit(0);
+            printf("myShell~:$ ");
         }
+        
+        else
+        {
+            int pid = fork();
+            PARENT_PID = getpid();
 
-        wait(NULL);
+            if(pid == 0)
+            {
+                //Inside Child Process
+                CURR_CHILD_PID = getpid();
+                char* op;
+                int ARG_LENGTH = get_args(buf) + 1;
+                char** arg = (char**) malloc (ARG_LENGTH*sizeof(char*));
+                for(int i=0; i<ARG_LENGTH; i++)
+                    arg[i] = (char*) malloc (70*sizeof(char));
+
+                int count = 0;
+                int j=0;
+                for(int i=0; buf[i]!='\0'; i++)
+                {
+                    if(buf[i] == ' ')
+                    {
+                        arg[count][j] = '\0';
+                        j = 0;
+                        count ++;
+                    }
+
+                    else
+                        arg[count][j++] = buf[i];
+                }
+                
+                arg[count][j] = '\0';
+                arg[count+1] = NULL;
+
+                //Now, arg is a NULL terminated Array of Strings
+                int curr = 0;
+
+                while(curr < PATH_LENGTH)
+                {
+                    if(execv(str_concat(PATH[curr], arg[0]), arg) < 0)
+                    {
+                        //Try all possible outcomes in PATH until it executes
+                        curr ++;
+                        if(curr == PATH_LENGTH)
+                        {
+                            perror("Error");
+                            exit(0);
+                        }
+                    }
+                }
+                free(op);
+                for(int i=0; i<ARG_LENGTH; i++)
+                {
+                    free(arg[i]);
+                }
+                free(arg);
+                perror("Error:");
+                exit(0);
+            }
+            else
+            {
+                wait(NULL);
+                printf("myShell~:$ ");
+                CURR_CHILD_PID = -1;
+            }
+        }
     }
 
     free(buf);
-
     return 0;
 }
